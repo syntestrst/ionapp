0a63aa2bbd5d7c4c2a59c9bef92d7dfa72d0b353
bastien <basien.guichard@gmail.com>
2015-08-08 15:10:19 +0200

    Clean change IDE

--- www/js/constants.js
+++ www/js/constants.js
@@ -2,4 +2,13 @@
  * Created by test on 24/06/2015.
  */
 
-angular.module('ionapp.constants',[]).constant('facebookAppId','1379299842395038');
+angular.module('ionapp.constants', []).constant('facebookAppId', '1379299842395038');
+
+
+
+function hello (parameters){
+    var name = parameters.name; //object
+
+    console.log("Salut" + name);
+
+}
